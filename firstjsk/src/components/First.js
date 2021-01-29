43ffa409d9a109158cabec9e8b4aa016047854e7
AseelAlMakhamrah <aseelmakhamrah98@gmail.com>
2021-01-29 23:30:25 +0200

    First Assignment on React

---
+++ firstjsk/src/components/First.js
@@ -0,0 +1,19 @@
+import React, { Component} from 'react';
+
+class First extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World!</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt.Everst</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        );
+    }
+}
+export default First;
